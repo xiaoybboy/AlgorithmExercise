eb2e37b3fd22dc9fadf3a0d8e3aed3d8bcf59d1b
XIAOMOZI <1737288636qq.com>
2017-06-20 13:22:19 +0800

    同步

--- src/com/baseAlgorithm/BubbleSort.java
+++ src/com/baseAlgorithm/BubbleSort.java
@@ -2,33 +2,31 @@ package com.baseAlgorithm;
 
 /**
  * 冒泡排序
- * 
- * @author XIAO
  *
+ * @author XIAO
  */
 public class BubbleSort {
-	// 冒泡排序
-	public static int[] bubbleSort(int[] data) {
-		int length = data.length;
-		for (int i = 0; i < length - 1; i++) {
-			for (int j = 0; j < length - 1 - i; j++) {
-				// 如果data[j]和data[j+1]反向
-				if (data[j] > data[j + 1]) {
-					int temp = data[j];
-					data[j] = data[j + 1];
-					data[j + 1] = temp;
-				}
-			}
-		}
-		return data;
-	}
+    // 冒泡排序
+    public static int[] bubbleSort(int[] data) {
+        int length = data.length;
+        for (int i = 0; i < length - 1; i++) {
+            // 如果data[j]和data[j+1]反向
+            for (int j = 0; j < length - 1 - i; j++)
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+        }
+        return data;
+    }
 
-	public static void main(String[] args) {
-		int[] data = { 67, 69, 75, 87, 89, 90, 99, 100 };
-		int[] sorted = BubbleSort.bubbleSort(data);
-		for (int i = 0; i < sorted.length - 1; i++) {
-			System.out.println(sorted[i]);
-		}
-	}
+    public static void main(String[] args) {
+        int[] data = {67, 69, 75, 87, 89, 90, 99, 100};
+        int[] sorted = BubbleSort.bubbleSort(data);
+        for (int i = 0; i < sorted.length - 1; i++) {
+            System.out.println(sorted[i]);
+        }
+    }
 
 }
---
+++ src/com/baseAlgorithm/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.baseAlgorithm;
+
+/**
+ * Created by XIAO on 2017/5/12.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
