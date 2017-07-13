7c1a712425659833c1288dce78c5230212ef9eff
XIAOMOZI <1737288636qq.com>
2017-07-13 17:37:19 +0800

    京东笔试

---
+++ src/com/company/jingdong/Candy.java
@@ -0,0 +1,123 @@
+package com.company.jingdong;
+
+import java.util.Scanner;
+
+/**
+ * 某糖果公司专门生产儿童糖果，它最受儿童欢迎的糖果有A1、A2两个序列，均采用盒式包装。包装好的A1类糖果体积为一个存储单位，而包装好的A2类糖果体积
+ * 正好是A1类的两倍。
+ * 
+ * 这两类糖果之所以广受儿童欢迎，是因为糖果中含有公司独家研发的魔幻因子。A1或A2序列中的糖果，看起来包装可能是一样的，但因为其中的魔幻因子含量
+ * 不同被细分为不同的产品。
+ * 
+ * 临近传统节日，公司的糖果供不应求。作为一个精明的糖果分销商，小东希望能够借此大赚一笔，于是带着现金开着货车来公司提货。货车的容量是确定的，
+ * 小东希望采购的糖果能够尽可能装满货车，且糖果的魔幻因子总含量最高。只要不超出货车容量，糖果总可以装入货车中。
+ * 
+ * 小东希望你能帮她解决这一问题。
+ * 
+ * 输入
+ * 
+ * 输入中有多组测试数据。每组测试数据的第一行有两个整数n和v，1<=n<=10^5, 1<=v<=10^9，n为可供选购糖果数量，v为货车的容量。
+ * 
+ * 随后n行为糖果的具体信息，第一行编号为1，第二行编号为2，以此类推，最后一行编号为n。每行包含两个整数ti和pi，1<=ti<=2,
+ * 1<=pi<=10^4，ti为糖果所属的序列，1为A1、2为A2，pi则是其中的魔幻因子含量。
+ * 
+ * 样例输入
+ * 
+ * 3 2
+ * 
+ * 1 2
+ * 
+ * 2 7
+ * 
+ * 1 3
+ * 
+ * 输出
+ * 对每组测试数据，先在单独的一行中输出能采购的糖果中的魔幻因子最高含量，之后在单独的行中按编号从小到大的顺序输出以空格分隔的糖果编号，若有多组糖果组合均能满足要求，输出编号最小的组。若没有糖果能够满足要求，则在第一行中输出0，第二行输出“No”。
+ * 样例输出
+ * 
+ * 7 2
+ * 
+ * @author XIAO
+ *
+ */
+public class Candy {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		while (cin.hasNextInt()) {
+			int n = cin.nextInt(), v = cin.nextInt();
+
+			int[] w = new int[n + 1];
+			int[] p = new int[n + 1];
+			int totalW = 0, totalP = 0;
+			for (int i = 1; i <= n; ++i) {
+				w[i] = cin.nextInt();
+				p[i] = cin.nextInt();
+				totalW += w[i];
+				totalP += p[i];
+			}
+
+			if (totalW <= v) {
+				System.out.println(totalP);
+				boolean f = true;
+				for (int i = 1; i <= n; ++i) {
+					if (f) {
+						f = false;
+					} else {
+						System.out.print(" ");
+					}
+					System.out.print(i);
+				}
+				System.out.println();
+			} else {
+
+				int[][] dp = new int[n + 1][v + 1];
+				int[] c = new int[n + 1];
+				int ymin = Math.min(w[1] - 1, v);
+				for (int j = 1; j <= ymin; ++j) {
+					dp[1][j] = 0;
+				}
+				for (int j = w[1]; j <= v; ++j) {
+					dp[1][j] = p[1];
+				}
+
+				for (int i = 2; i <= n; ++i) {
+					int min = Math.min(w[i] - 1, v);
+					for (int j = 1; j <= min; ++j) {
+						dp[i][j] = dp[i - 1][j];
+					}
+					for (int j = w[i]; j <= v; ++j) {
+						dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + p[i]);
+					}
+				}
+
+				if (dp[n][v] == 0) {
+					System.out.println(0);
+					System.out.println("No");
+				} else {
+					System.out.println(dp[n][v]);
+					for (int i = n, j = v; i > 0; --i) {
+						if (dp[i][j] == dp[i - 1][j]) {
+							c[i] = 0;
+						} else {
+							c[i] = 1;
+							j -= w[i];
+						}
+					}
+					boolean f = true;
+					for (int i = 1; i <= n; ++i) {
+						if (c[i] == 1) {
+							if (f) {
+								f = false;
+							} else {
+								System.out.printf(" ");
+							}
+							System.out.print(i);
+						}
+					}
+					System.out.println();
+				}
+			}
+		}
+		cin.close();
+	}
+}
--- src/com/company/jingdong/TradeBill.java
+++ src/com/company/jingdong/TradeBill.java
@@ -2,19 +2,19 @@ package com.company.jingdong;
 
 import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.Stack;
 import java.util.TreeMap;
 
 /**
- * 金融证券行业超好的薪酬待遇，吸引了大批的求职者前往应聘，小东也不例外，准备应聘一家证券公司。面试官为考察她的数据分析、处理和编码能力，为她准备了以下问题。
+ * 金融证券行业超好的薪酬待遇，吸引了大批的求职者前往应聘，小东也不例外，准备应聘一家证券公司。面试官为考察她的数据分析、处理和编码能力，
  * 
- * 股票交易中，委托是指股票交易者在证券公司买卖股票。每手委托包括一个委托单号i、价格pi、买入或卖出标记di及交易数量qi。
+ * 为她准备了以下问题。股票交易中，委托是指股票交易者在证券公司买卖股票。每手委托包括一个委托单号i、价格pi、买入或卖出标记di及交易数量qi。
  * 
  * 交易处理中，需要把同类业务（买入或卖出）中相同价格的所有委托合并起来，形成一个清单。清单的第一部分为按价格降序排列的合并后的卖出委托，
  * 紧随其后的是按相同顺序排列的买入合并委托。证券公司比较关心的是比较抢手的s条合并委托信息，
  * 
- * 需要得到买入及卖出最抢手的s条合并委托。对于买入委托，抢手的是指报价高的委托，而卖出委托中报价低的较为抢手。若买或卖的合并委托数小于s条，则全部列入清单中。
+ * 需要得到买入及卖出最抢手的s条合并委托。对于买入委托，抢手的是指报价高的委托，而卖出委托中报价低的较为抢手。若买或卖的合并委托数小于s条，
+ * 
+ * 则全部列入清单中。
  * 
  * 现在小东拿到的是n个委托，请你帮忙找出最抢手的s个合并委托。
  * 
@@ -60,66 +60,58 @@ import java.util.TreeMap;
 public class TradeBill {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		while (sc.hasNext()) {
-			int n = sc.nextInt();
-			int s = sc.nextInt();
+		while (sc.hasNextInt()) {
+			int n = sc.nextInt();// 委托数
+			int s = sc.nextInt();// 抢手清单数
 
-			TreeMap<Integer, Integer> mapB = new TreeMap<>();
-			TreeMap<Integer, Integer> mapS = new TreeMap<>();
-			while ((n--) > 0) {
-
-				String sb = sc.next();
-				if (sb.equals("B")) {
-					int money = sc.nextInt();
-					int time = sc.nextInt();
-					if (mapB.containsKey(money)) {
-						mapB.put(money, time + mapB.get(money));
-					} else
-						mapB.put(money, time);
-				} else {
-					int money = sc.nextInt();
-					int time = sc.nextInt();
-					if (mapS.containsKey(money)) {
-						mapS.put(money, time + mapS.get(money));
-					} else
-						mapS.put(money, time);
+			TreeMap<Integer, Integer> buyList = new TreeMap<>();
+			TreeMap<Integer, Integer> sellList = new TreeMap<>();
+			for (int i = 0; i < n; i++) {
+				String buyOrSell = sc.next();
+				if (buyOrSell.equals("B")) {
+					int price = sc.nextInt();
+					int num = sc.nextInt();
+					if (buyList.containsKey(price)) {
+						buyList.put(price, num + buyList.get(price));
+					} else {
+						buyList.put(price, num);
+					}
+				} else if (buyOrSell.equals("S")) {
+					int price2 = sc.nextInt();
+					int num2 = sc.nextInt();
+					if (sellList.containsKey(price2)) {
+						sellList.put(price2, num2 + sellList.get(price2));
+					} else {
+						sellList.put(price2, num2);
+					}
 				}
-
 			}
 
-			Stack<String> stack = new Stack<>();
-
-			Set<Integer> sets = mapS.keySet();
-			Iterator<Integer> its = sets.iterator();
+			// 输出结果
+			Iterator<Integer> it = sellList.descendingKeySet().iterator();// 反向遍历
 			int count = 0;
-			while (its.hasNext()) {
-				int money = (Integer) its.next();
-				int time = mapS.get(money);
-
-				if (count++ < s)
-					stack.add("S " + money + " " + time);
-				else
+			while (it.hasNext()) {
+				int price = it.next();
+				int num = sellList.get(price);
+				System.out.println("S " + price + " " + num);
+				count++;
+				if (count == s) {
 					break;
-			}
-			while (!stack.isEmpty()) {
-				System.out.println(stack.pop());
+				}
 			}
 
-			Set<Integer> setb = mapB.keySet();
-			Iterator<Integer> itb = setb.iterator();
-			count = 0;
-			while (itb.hasNext()) {
-				int money = (Integer) itb.next();
-				int time = mapB.get(money);
-
-				if (count++ >= mapB.size() - s)
-					stack.add("B " + money + " " + time);
-			}
-			while (!stack.isEmpty()) {
-				System.out.println(stack.pop());
+			// 输出结果
+			Iterator<Integer> it2 = buyList.descendingKeySet().iterator();
+			int count2 = 0;
+			while (it2.hasNext()) {
+				int price2 = it2.next();
+				int num2 = buyList.get(price2);
+				System.out.println("B " + price2 + " " + num2);
+				count2++;
+				if (count2 == s) {
+					break;
+				}
 			}
 		}
-		sc.close();
 	}
-
 }
