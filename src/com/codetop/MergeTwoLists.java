f9c35ba860bece5c91a2986a48965146cf26284c
xiaoybboy <1737288636@qq.com>
2022-10-17 22:06:24 +0800

    exce

---
+++ src/com/codetop/MergeTwoLists.java
@@ -0,0 +1,35 @@
+package com.codetop;
+
+import com.model.ListNode;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+public class MergeTwoLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //result 指向头节点，cur指向当前节点
+        ListNode result = new ListNode(0);
+        ListNode cur = result;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                cur.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+
+        if (list1 != null) {
+            cur.next = list1;
+        }
+
+        if (list2 != null) {
+            cur.next = list2;
+        }
+        return result.next;
+    }
+}
