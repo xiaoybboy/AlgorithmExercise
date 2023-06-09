76303ced85d1ef837978fdd976e87f60675ec0a7
xiaoybboy <1737288636@qq.com>
2023-06-09 20:23:03 +0800

    提交代码

---
+++ src/com/codetop/ReverseBetweenListNode.java
@@ -0,0 +1,36 @@
+package com.codetop;
+
+import com.model.ListNode;
+
+/**
+ * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+ */
+public class ReverseBetweenListNode {
+
+    /**
+     * 反转链表（头插法）
+     *
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 设置 dummyNode 是这一类问题的一般做法
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode cur = pre.next;
+        ListNode next;
+        for (int i = 0; i < right - left; i++) {
+            next = cur.next;
+            cur.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return dummyNode.next;
+    }
+}
