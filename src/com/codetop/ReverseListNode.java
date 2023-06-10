08bf521c0a0ffa9b2aa37df5e0b9ffe589133459
xiaoybboy <1737288636@qq.com>
2023-06-10 09:24:20 +0800

    提交代码

--- src/com/codetop/ReverseListNode.java
+++ src/com/codetop/ReverseListNode.java
@@ -7,16 +7,27 @@ import com.model.ListNode;
  */
 public class ReverseListNode {
 
-    public ListNode reverseList(ListNode head) {
+    public static ListNode reverseList(ListNode head) {
         ListNode pre = null;
         ListNode cur = head;
 
         while (cur != null) {
-           ListNode next = cur.next;
-           cur.next = pre;
-           pre = cur;
-           cur = next;
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
         }
         return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        ListNode result = ReverseListNode.reverseList(node1);
     }
 }
